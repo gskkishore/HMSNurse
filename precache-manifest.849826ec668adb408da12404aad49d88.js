674835060f65489be0995464bf100d57c7d972ac
kishore <gskkishore39@gmail.com>
2021-08-11 17:00:01 +0530

    Updates

---
+++ precache-manifest.849826ec668adb408da12404aad49d88.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9f44d1b27efbd06ecb73b165edc723",
+    "url": "/HMSNurse/nursemodule/index.html"
+  },
+  {
+    "revision": "d9c65fb321fc2f9e900d",
+    "url": "/HMSNurse/nursemodule/static/css/2.8541c9cf.chunk.css"
+  },
+  {
+    "revision": "ae3d3a821ea6f3ae7f48",
+    "url": "/HMSNurse/nursemodule/static/css/main.8ce55ecf.chunk.css"
+  },
+  {
+    "revision": "d9c65fb321fc2f9e900d",
+    "url": "/HMSNurse/nursemodule/static/js/2.5c417421.chunk.js"
+  },
+  {
+    "revision": "ae3d3a821ea6f3ae7f48",
+    "url": "/HMSNurse/nursemodule/static/js/main.3173b6ad.chunk.js"
+  },
+  {
+    "revision": "48fcbf545d1960486860",
+    "url": "/HMSNurse/nursemodule/static/js/runtime-main.cd485395.js"
+  },
+  {
+    "revision": "9c2f3f7d50e699c643ce6a3fe563d6a1",
+    "url": "/HMSNurse/nursemodule/static/media/11.9c2f3f7d.jpg"
+  },
+  {
+    "revision": "6367e28060c28731cc4d7e3908ac8ee9",
+    "url": "/HMSNurse/nursemodule/static/media/Full.6367e280.svg"
+  },
+  {
+    "revision": "ac8cde109fce4d59795e9c87bfe4495d",
+    "url": "/HMSNurse/nursemodule/static/media/Half.ac8cde10.svg"
+  },
+  {
+    "revision": "c4b9bb1a71b1b74c3a364197c10b37f4",
+    "url": "/HMSNurse/nursemodule/static/media/Logo.c4b9bb1a.png"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/HMSNurse/nursemodule/static/media/Roboto-Regular.5673da52.ttf"
+  },
+  {
+    "revision": "2f0195aab755612b7733cc305689a735",
+    "url": "/HMSNurse/nursemodule/static/media/advertisebooking.2f0195aa.svg"
+  },
+  {
+    "revision": "ba8d43f1d313439eca4525ee5e603c12",
+    "url": "/HMSNurse/nursemodule/static/media/availability.ba8d43f1.svg"
+  },
+  {
+    "revision": "9b34271c0db9fbfe629226a2a4a867e1",
+    "url": "/HMSNurse/nursemodule/static/media/avatar.9b34271c.jpg"
+  },
+  {
+    "revision": "abdff9561587b7ace31dcb159615c23b",
+    "url": "/HMSNurse/nursemodule/static/media/back.abdff956.svg"
+  },
+  {
+    "revision": "c01efda92f58e18f1b428b14ec3a297c",
+    "url": "/HMSNurse/nursemodule/static/media/cancelledappointments.c01efda9.svg"
+  },
+  {
+    "revision": "fcb0747bb1f8caa619d7f8bd2b77804a",
+    "url": "/HMSNurse/nursemodule/static/media/dashboard.fcb0747b.svg"
+  },
+  {
+    "revision": "644f8adb75c253d0228525e05b1475dd",
+    "url": "/HMSNurse/nursemodule/static/media/deals.644f8adb.svg"
+  },
+  {
+    "revision": "05628ab5de527d6d003b0c96740695fa",
+    "url": "/HMSNurse/nursemodule/static/media/excel.05628ab5.svg"
+  },
+  {
+    "revision": "5791519f1ec8cbc8b3234a6603ca2f1e",
+    "url": "/HMSNurse/nursemodule/static/media/eye.5791519f.svg"
+  },
+  {
+    "revision": "3df2480fe927f338e5838b4eb0a614e4",
+    "url": "/HMSNurse/nursemodule/static/media/green.3df2480f.svg"
+  },
+  {
+    "revision": "e7277258c7f954ff7fad073be5d81ddf",
+    "url": "/HMSNurse/nursemodule/static/media/idlenurse.e7277258.svg"
+  },
+  {
+    "revision": "427c27267d16fd8f2ba763ca473dad0f",
+    "url": "/HMSNurse/nursemodule/static/media/inbox.427c2726.svg"
+  },
+  {
+    "revision": "4bf897072f8742d4fccfc29f44c18aa8",
+    "url": "/HMSNurse/nursemodule/static/media/info.4bf89707.svg"
+  },
+  {
+    "revision": "7f6b6825791321a2565a2cce08485e56",
+    "url": "/HMSNurse/nursemodule/static/media/managenurse.7f6b6825.svg"
+  },
+  {
+    "revision": "9c2f3f7d50e699c643ce6a3fe563d6a1",
+    "url": "/HMSNurse/nursemodule/static/media/nurse.9c2f3f7d.jpg"
+  },
+  {
+    "revision": "b93ceec8795403471d8f73ac7264b4c3",
+    "url": "/HMSNurse/nursemodule/static/media/nurse_login.b93ceec8.png"
+  },
+  {
+    "revision": "3278ee11bf7f20e5dd7f0cf3b541a009",
+    "url": "/HMSNurse/nursemodule/static/media/nursehired.3278ee11.svg"
+  },
+  {
+    "revision": "7b09e37f4c8955dacdb3707f55e2f865",
+    "url": "/HMSNurse/nursemodule/static/media/nurseonleave.7b09e37f.svg"
+  },
+  {
+    "revision": "860905102c3dd573b123b21efeb7c56b",
+    "url": "/HMSNurse/nursemodule/static/media/okay.86090510.PNG"
+  },
+  {
+    "revision": "a80011136d7bbfb4970914c169303c88",
+    "url": "/HMSNurse/nursemodule/static/media/pdf.a8001113.svg"
+  },
+  {
+    "revision": "54fe904936264587a4e5f6e988c57974",
+    "url": "/HMSNurse/nursemodule/static/media/print.54fe9049.svg"
+  },
+  {
+    "revision": "767ce51005f851b377259d58e22ad243",
+    "url": "/HMSNurse/nursemodule/static/media/profile.767ce510.svg"
+  },
+  {
+    "revision": "354c521c58f842305e950640cfc4a973",
+    "url": "/HMSNurse/nursemodule/static/media/report.354c521c.svg"
+  },
+  {
+    "revision": "ffd19726217de3de38c15fe877e2f981",
+    "url": "/HMSNurse/nursemodule/static/media/revenue.ffd19726.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/HMSNurse/nursemodule/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/HMSNurse/nursemodule/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/HMSNurse/nursemodule/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "8d96cfbd9392828838d8c897d34e7788",
+    "url": "/HMSNurse/nursemodule/static/media/right.8d96cfbd.svg"
+  },
+  {
+    "revision": "b900586b9121ad270b36efd9699a6a66",
+    "url": "/HMSNurse/nursemodule/static/media/workflow.b900586b.svg"
+  }
+]);
